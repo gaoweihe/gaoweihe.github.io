5e3e46d8f1a6183ef444baad8784c81e46ea856e
GAO Weihe <gao.weihe@outlook.com>
2024-12-05 07:41:18 +0000

    restructure footer

---
+++ js/element-ui-app.js
@@ -0,0 +1,29 @@
+const { createApp, ref, onMounted } = Vue;
+
+const App = {
+    setup() {
+        const words = ["chubby", "fluffy", "adorable", "cute"];
+        const fox_adjective = ref(words[0]);
+        let index = 0;
+
+        const swapWord = () => {
+        index = (index + 1) % words.length;
+        fox_adjective.value = words[index]; 
+        };
+
+        onMounted(() => {
+        setInterval(swapWord, 300); // Swap word every 0.5 seconds
+        });
+
+        return {
+        fox_adjective, 
+        activeNames: ["1"],
+        projects_active_names: ["1", "2", "3", "4", "5"],
+        travelogue_active_names: [],
+        };
+    },
+    methods: {},
+};
+const app = Vue.createApp(App);
+app.use(ElementPlus, {});
+app.mount("#element-ui-app");
