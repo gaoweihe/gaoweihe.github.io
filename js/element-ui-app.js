098ed1083c1d5333a121f9391f48ad4c2e37407e
GAO Weihe <gao.weihe@outlook.com>
2024-12-12 04:34:01 +0000

    add latency masks; chore

--- js/element-ui-app.js
+++ js/element-ui-app.js
@@ -1,56 +1,68 @@
 const { createApp, ref, onMounted } = Vue;
 
 const App = {
-    setup() {
-        const words = ["chubby", "fluffy", "plumpy", "bouncy", "plushy"];
-        const fox_adjective = ref(words[0]);
-        let index = 0;
+  setup() {
+    const words = ["chubby", "fluffy", "plumpy", "bouncy", "plushy"];
+    const fox_adjective = ref(words[0]);
+    let index = 0;
 
-        const swapWord = () => {
-            index = (index + 1) % words.length;
-            fox_adjective.value = words[index]; 
-        };
+    const swapWord = () => {
+      index = (index + 1) % words.length;
+      fox_adjective.value = words[index];
+    };
 
-        const research_topics = {
-            "ML/AI": "#409EFF", 
-            "Logs": "#67C23A", 
-            "Sec": "#E6A23C", 
-            "Crypto": "#F56C6C", 
-            "IoT": "#13C2C2", 
-            "Sched": "#F8D02A", 
-            "Img": "#9C47E2", 
-            "BME": "#FF8C44", 
-            "TEE": "#1D9C99", 
-            "OLAP": "#D4356C", 
-            "CivilEng": "#1D9C99",
-            "MechEng": "#2F54EB",
-            "SmartCities": "#F4A300", 
-        }
+    const research_topics = {
+      "ML/AI": "#409EFF",
+      "Logs": "#67C23A",
+      "Sec": "#E6A23C",
+      "Crypto": "#F56C6C",
+      "IoT": "#13C2C2",
+      "Sched": "#F8D02A",
+      "Graphics": "#9C47E2",
+      "BME": "#FF8C44",
+      "TEE": "#1D9C99",
+      "OLAP": "#D4356C",
+      "CivilEng": "#1D9C99",
+      "MechEng": "#2F54EB",
+      "SmartCities": "#F4A300",
+    };
 
-        const getResearchTopicTagStyle = (topic_name) => {
-            color = research_topics[topic_name];
-            border_color = color;
-            return `color: ${color}; border-color: ${border_color};`;
-        }
+    const getResearchTopicTagStyle = (topic_name) => {
+      color = research_topics[topic_name];
+      border_color = color;
+      return `color: ${color}; border-color: ${border_color};`;
+    };
 
-        onMounted(() => {
-        setInterval(swapWord, 300); // Swap word every 0.3 seconds
-        const tagElements = document.querySelectorAll('.topic-tag');
-        tagElements.forEach(tag => {
-            topic_name = tag.getAttribute('topic');
-            tag_style = getResearchTopicTagStyle(topic_name);
-            tag.setAttribute('style', tag_style);
-            console.log(tag_style);
-        })
-        });
+    const is_loaded = ref(false);
 
-        return {
-        fox_adjective, 
-        activeNames: ["1"],
-        projects_active_names: ["1", "2", "3", "4", "5"],
-        travelogue_active_names: [],
-        };
-    },
+    onMounted(() => {
+      setInterval(swapWord, 300); // Swap word every 0.3 seconds
+      const tagElements = document.querySelectorAll(".topic-tag");
+      tagElements.forEach((tag) => {
+        topic_name = tag.getAttribute("topic");
+        tag_style = getResearchTopicTagStyle(topic_name);
+        tag.setAttribute("style", tag_style);
+      });
+      setTimeout(() => {
+        document.documentElement.style.setProperty(
+          "--hidden-before-vue",
+          "visible"
+        );
+      }, 200);
+      setTimeout(() => {
+        is_loaded.value = true;
+      }, 400);
+    });
+
+    return {
+      is_loaded: is_loaded,
+      message: "Hello World!",
+      fox_adjective,
+      activeNames: ["1"],
+      projects_active_names: ["1", "2", "3", "4", "5"],
+      travelogue_active_names: [],
+    };
+  },
 };
 const app = Vue.createApp(App);
 app.use(ElementPlus, {});
